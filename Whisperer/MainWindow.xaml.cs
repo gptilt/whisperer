746ba680568c7da7d7ba28c8941b791557cd8ba4
franciscoabsampaio <franciscoabsampaio@protonmail.com>
2026-01-11 20:12:14 +0000

    Initial commit

---
+++ Whisperer/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace Whisperer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CheckForLeague();
+        }
+
+        private void CheckForLeague()
+        {
+            // This looks for the process "LeagueClientUx" (The UI, not the game engine)
+            var leagueProcesses = Process.GetProcessesByName("LeagueClientUx");
+
+            if (leagueProcesses.Length > 0)
+            {
+                var p = leagueProcesses[0];
+                MessageBox.Show($"Success! League is running.\nProcess ID: {p.Id}\nWe are ready to connect.");
+            }
+            else
+            {
+                MessageBox.Show("League Client is not running. Open League and restart this app.");
+            }
+        }
+    }
+}
